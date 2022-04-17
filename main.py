4d49f7242baadb2ab446cfb1c7d4eafd86c8ca3a
Daníel Eldjárn <daniel@eldjarn.io>
2022-04-17 20:39:11 +0000

    Fix bug

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def main() -> None:
     dp.add_handler(CommandHandler("play", play))
 
     # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
+    # dp.add_handler(MessageHandler(Filters.text, echo))
 
     # log all errors
     dp.add_error_handler(error)
