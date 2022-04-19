83e2cd4e090591e8a9a56bd5b5676f44dfc6c768
danieleldjarn <daniel@eldjarn.io>
2022-04-19 00:51:29 +0000

    Feature/basic tradle func (#1)
    * Add Tradle play command

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import logging
 import os
 
 from dotenv import load_dotenv
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram.ext import Updater, CommandHandler, CallbackContext
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -21,45 +21,29 @@ if not is_prod:
 TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
 PORT =  int(os.environ.get('PORT', '8443'))
 
-# Define a few command handlers. These usually take the two arguments update and
-# context. Error handlers also receive the raised TelegramError object in error.
-def start(update: Updater, context: CallbackContext) -> None:
-    """Send a message when the command /start is issued."""
-    update.message.reply_text('Hi!')
 
-def help(update: Updater, context: CallbackContext) -> None:
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
-
-def echo(update: Updater, context: CallbackContext) -> None:
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
+def play(update: Updater, context: CallbackContext) -> None:
+    update.message.reply_text("Don't forget to play Tradle today! https://oec.world/en/tradle/")
+    
 
 def error(update: Updater, context: CallbackContext) -> None:
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
+
 
 def main() -> None:
     """Start the bot."""
     # Create the Updater and pass it your bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
     # Post version 12 this will no longer be necessary
-    updater = Updater(TOKEN, use_context=True)
+    updater = Updater(TOKEN)
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
 
-    # on different commands - answer in Telegram
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
-
-    # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
-
-    # log all errors
+    dp.add_handler(CommandHandler("play", play))
     dp.add_error_handler(error)
 
-    # Start the Bot
     updater.start_webhook(
         listen='0.0.0.0',
         port=PORT,
@@ -67,9 +51,6 @@ def main() -> None:
         webhook_url='https://tradlebot.herokuapp.com/' + TOKEN,
     )
 
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
 
 if __name__ == '__main__':
